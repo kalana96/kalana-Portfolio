8192d3c99a82af3279c4d7c23bdb25cf22cb92d0
kalana96 <70446964+kalana96@users.noreply.github.com>
2024-08-22 16:34:05 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+// import { Connect } from "vite";
 import "./App.css";
 import Hero from "./components/Hero/Hero";
 import Projects from "./components/Projects/Projects";
 import Skills from "./components/Skills/Skills";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -9,6 +12,8 @@ function App() {
       <Hero />
       <Projects />
       <Skills />
+      <Contact />
+      <Footer />
     </>
   );
 }
