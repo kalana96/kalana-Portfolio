7ddab2c5c1f51063753591666f4ad17724594e11
kalana96 <70446964+kalana96@users.noreply.github.com>
2024-08-26 21:36:46 +0530

    update hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar.jsx";
-import Hero from "./components/hero/Hero.jsx";
-import About from "./components/about/About.jsx";
+import Hero from "./components/Hero/Hero.jsx";
+import About from "./components/About/About.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import Skills from "./components/Skills/Skills.jsx";
-import Services from "./components/services/Services.jsx";
+import Services from "./components/Services/Services.jsx";
 import Contact from "./components/Contact/Contact.jsx";
 import Education from "./components/Education/Educaion.jsx";
 import Projects from "./components/Projects/Projects.jsx";
-// import Experience from "./components/Experince/Experiences.jsx";
 
 const App = () => {
   return (
